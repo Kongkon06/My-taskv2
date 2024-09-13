903ca999a53eb961e7241f408c92c8c6d4ab47b6
Kongkon Bora <kboar3525@gmail.com>
2024-09-13 18:37:06 +0530

    sigin button

--- Mytask-Frontend/src/Components/Appbar.tsx
+++ Mytask-Frontend/src/Components/Appbar.tsx
@@ -1,11 +1,20 @@
 import { useNavigate } from "react-router-dom"
 import DrawerNavigation from "./Sidebar";
+import { useRecoilState } from "recoil";
+import { parentid } from "../Atoms/Atoms";
 
 export const Appbar = ()=>{
     const navigate = useNavigate();
-    return<div  className="w-sm flex justify-between items-center mb-5 p-2 border-b border-blue-800 bg-slate-950 p-2 sm:w-full border-b bg-indigo-950 flex justify-center md:text-lg lg:text-3xl">
+    const [userid,setid] = useRecoilState(parentid);
+    return<div  className="w-sm flex justify-between items-center mb-5 p-2 border-b border-blue-800 bg-slate-950 p-2 sm:w-full border-b bg-indigo-950 flex justify-center ">
         <div className="w-md"><DrawerNavigation/></div>
-        <div onClick={()=>{navigate('/')}} className=" font-semibold text-white cursor-pointer font-dm-sans">My Task</div>
-        <div className="w-md text-md text-white cursor-pointer">{`(▀̿ĺ̯▀̿ ̿)`}</div>
+        <div onClick={()=>{navigate('/')}} className=" font-semibold text-white cursor-pointer font-dm-sans md:text-lg lg:text-3xl">My Task</div>
+        <div role="button" onClick={()=>{
+            if(userid === 0){
+                setid(0)
+            }
+            navigate('/signin');
+
+        }} className="w-md font-dm-sans rounded-3xl bg-slate-700 mr-2 px-2 py-1 text-md font-semibold text-white cursor-pointer">{userid === 0?"Signin":"Logout"}</div>
     </div>
 }
